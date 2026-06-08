d667671c372a00c27945a0056e31d672f5ee86ad
Ibar-Estudios <ibarestudioscaubet@gamil.com>
2026-06-08 20:03:15 -0300

    fix express 5 wildcard

--- Proyecto-UX-UI-Deploy/Backend/server.js
+++ Proyecto-UX-UI-Deploy/Backend/server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 app.use('/api/ux-sections', require('./routes/uxSections'));
 
 // 404 handler
-app.use('*', (req, res) => res.status(404).json({ error: 'Ruta no encontrada' }));
+app.use((req, res) => res.status(404).json({ error: 'Ruta no encontrada' }));
 
 // Error handler
 app.use((err, req, res, next) => {
